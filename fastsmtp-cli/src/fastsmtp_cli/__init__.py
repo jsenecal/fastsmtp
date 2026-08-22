589e5f1898574013ec4195a948ee78ec30d8e987
Jonathan Senecal <jonathan.senecal@metrooptic.com>
2026-08-22 16:31:59 +0000

    chore: bump version 0.1.6 -> 0.2.0

--- fastsmtp-cli/src/fastsmtp_cli/__init__.py
+++ fastsmtp-cli/src/fastsmtp_cli/__init__.py
@@ -1,3 +1,3 @@
 """FastSMTP CLI - Remote CLI client for FastSMTP server."""
 
-__version__ = "0.1.6"
+__version__ = "0.2.0"
--- fastsmtp/src/fastsmtp/__init__.py
+++ fastsmtp/src/fastsmtp/__init__.py
@@ -1,3 +1,3 @@
 """FastSMTP - SMTP-to-Webhook Relay Server."""
 
-__version__ = "0.1.6"
+__version__ = "0.2.0"
